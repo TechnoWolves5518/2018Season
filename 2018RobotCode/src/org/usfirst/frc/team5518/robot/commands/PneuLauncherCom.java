06f9f917ab6e1ccd1c5bf920889ea1dd2e4f7a6e
boktah <boktah2@gmail.com>
2018-02-12 09:55:34 -0500

    pneulauncher updated (bugs)

--- 2018RobotCode/src/org/usfirst/frc/team5518/robot/commands/PneuLauncherCom.java
+++ 2018RobotCode/src/org/usfirst/frc/team5518/robot/commands/PneuLauncherCom.java
@@ -1,23 +1,54 @@
 package org.usfirst.frc.team5518.robot.commands;
 
+import org.usfirst.frc.team5518.robot.OI;
+import org.usfirst.frc.team5518.robot.Robot;
+import org.usfirst.frc.team5518.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
  *
  */
+
 public class PneuLauncherCom extends Command {
-	// hello
-    public PneuLauncherCom() {
+	
+	public boolean isAPressed, wasAPressed, isXPressed, wasXPressed;
+	
+	public PneuLauncherCom() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
+		// System.out.println("SUBSYSTEM NAME: " + Robot.sfSub.getName());
+		requires(Robot.sfSub);
+    	
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    		isAPressed = false;
+    		wasAPressed = false;
+    		isXPressed = false;
+    		wasXPressed = false;
+    		Robot.sfSub.initNeutral();
     }
-
+    
+  
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+	    	
+    		isAPressed = OI.sfController.getRawButton(RobotMap.XBOX_ABTN);
+	    	if (isAPressed != wasAPressed && isAPressed == true){
+	    		Robot.sfSub.shootSwitch();   		
+	    	}
+	    	wasAPressed = isAPressed;
+	    	
+	    	isXPressed = OI.sfController.getRawButton(RobotMap.XBOX_XBTN);
+	    	if (isXPressed != wasXPressed && isXPressed == true){
+	    		Robot.sfSub.shootScale();
+	    	}
+	    	wasXPressed = isXPressed;
+	    	
     }
 
     // Make this return true when this Command no longer needs to run execute()
