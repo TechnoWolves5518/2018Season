1abecd92d02a2d5ed340b4d9a5dc5abe6a524828
boktah <boktah2@gmail.com>
2018-02-20 17:18:33 -0500

    fixed indents

--- 2018RobotCode/src/org/usfirst/frc/team5518/robot/commands/autonomous/RotateDistance.java
+++ 2018RobotCode/src/org/usfirst/frc/team5518/robot/commands/autonomous/RotateDistance.java
@@ -7,10 +7,10 @@ import org.usfirst.frc.team5518.robot.Robot;
  *
  */
 public class RotateDistance extends Command {
-
+	
 	private float distance;
 	private float speed;
-
+	
 	public RotateDistance(float m_distance, float m_speed) {
 		// Use requires() here to declare subsystem dependencies
 		// eg. requires(chassis);
@@ -19,20 +19,20 @@ public class RotateDistance extends Command {
 		distance = m_distance;
 		speed = m_speed;
 	}
-
+	
 	// Called just before this Command runs the first time
 	protected void initialize() {
 		Robot.driveTrainSub.resetGyro();
 		Robot.driveTrainSub.pidGyro.setSetpoint(distance);
 	}
-
+	
 	// Called repeatedly when this Command is scheduled to run
 	protected void execute() {
 		Robot.logger.debug("running rotateDistance");
 		// Robot.driveTrainSub.autoRotate(distance, speed);
 		Robot.driveTrainSub.pidTurn(distance);
 	}
-
+	
 	// Make this return true when this Command no longer needs to run execute()
 	protected boolean isFinished() {
 		if (Robot.driveTrainSub.pidGyro.onTarget()) {
@@ -43,13 +43,13 @@ public class RotateDistance extends Command {
 			return false;
 		}
 	}
-
+	
 	// Called once after isFinished returns true
 	protected void end() {
 		Robot.logger.debug("Rotate Distance Ended!");
 		Robot.driveTrainSub.resetGyro();
 	}
-
+	
 	// Called when another command which requires one or more of the same
 	// subsystems is scheduled to run
 	protected void interrupted() {
