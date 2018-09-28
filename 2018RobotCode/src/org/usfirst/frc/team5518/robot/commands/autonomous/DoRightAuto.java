e102a11ecd62cd3d2edfdfa5f8117bd9b9bf8e28
Taha Bokhari <tbokhar@ncsu.edu>
2018-09-28 19:12:29 -0400

    Disabled scale auto for right paths

--- 2018RobotCode/src/org/usfirst/frc/team5518/robot/commands/autonomous/DoRightAuto.java
+++ 2018RobotCode/src/org/usfirst/frc/team5518/robot/commands/autonomous/DoRightAuto.java
@@ -108,22 +108,26 @@ public class DoRightAuto extends CommandGroup {
 	}
 
 	private void rightToRightScale() {
-		addSequential(new DriveDistance(300, RobotMap.AUTO_DRIVE_SPEED));
-		addSequential(new RotateDistance(-90, RobotMap.AUTO_ROTATE_SPEED));
-		addSequential(new DriveDistanceUltrasonic(30, RobotMap.AUTO_DRIVE_SPEED));
-		addSequential(new WaitCom(RobotMap.WAIT_TIME, false));
-		addSequential(new AutoLauncherCom(RobotMap.SCALE_DELAY)); // pass in delay for respective target
+//		addSequential(new DriveDistance(300, RobotMap.AUTO_DRIVE_SPEED));
+//		addSequential(new RotateDistance(-90, RobotMap.AUTO_ROTATE_SPEED));
+//		addSequential(new DriveDistanceUltrasonic(30, RobotMap.AUTO_DRIVE_SPEED));
+//		addSequential(new WaitCom(RobotMap.WAIT_TIME, false));
+//		addSequential(new AutoLauncherCom(RobotMap.SCALE_DELAY)); // pass in delay for respective target
+		
+		rightToLine();
 	}
 
 	private void rightToLeftScale() {
-		addSequential(new DriveDistance(220, RobotMap.AUTO_DRIVE_SPEED)); // First leg gets us past the left side of switch
-		addSequential(new RotateDistance(-90, RobotMap.AUTO_ROTATE_SPEED));
-		addSequential(new DriveDistance(224, RobotMap.AUTO_DRIVE_SPEED)); // Second leg drives across field to right side
-		addSequential(new RotateDistance(90, RobotMap.AUTO_ROTATE_SPEED));
-		addSequential(new DriveDistance(68, RobotMap.AUTO_DRIVE_SPEED)); // Third leg gets us even with scale
-		addSequential(new RotateDistance(65, RobotMap.AUTO_ROTATE_SPEED));
-		addSequential(new WaitCom(RobotMap.WAIT_TIME, false));
-		addSequential(new AutoLauncherCom(RobotMap.SCALE_DELAY)); // pass in delay for respective target
+//		addSequential(new DriveDistance(220, RobotMap.AUTO_DRIVE_SPEED)); // First leg gets us past the left side of switch
+//		addSequential(new RotateDistance(-90, RobotMap.AUTO_ROTATE_SPEED));
+//		addSequential(new DriveDistance(224, RobotMap.AUTO_DRIVE_SPEED)); // Second leg drives across field to right side
+//		addSequential(new RotateDistance(90, RobotMap.AUTO_ROTATE_SPEED));
+//		addSequential(new DriveDistance(68, RobotMap.AUTO_DRIVE_SPEED)); // Third leg gets us even with scale
+//		addSequential(new RotateDistance(65, RobotMap.AUTO_ROTATE_SPEED));
+//		addSequential(new WaitCom(RobotMap.WAIT_TIME, false));
+//		addSequential(new AutoLauncherCom(RobotMap.SCALE_DELAY)); // pass in delay for respective target
+		
+		rightToLine();
 	}
 
 }
